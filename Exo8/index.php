e4aa7059ccded85976a3a9d1b679f82644f16dca
December81 <megastudio@outlook.fr>
2020-12-16 10:37:19 +0100

    ok

--- Exo8/index.php
+++ Exo8/index.php
@@ -7,6 +7,7 @@ if(empty($_GET['gender']) && empty($_GET['firstName']) && empty($_GET['lastName'
 
     ?>
 <form action="index.php" method="get">
+
     <select name="gender" id="gender">
         <option value=""> Votre sexe ?</option>
         <option value="Homme">Homme</option>
@@ -17,38 +18,37 @@ if(empty($_GET['gender']) && empty($_GET['firstName']) && empty($_GET['lastName'
     <input type="text" placeholder="Nom" name="lastName">
     <input type="file" name="file">
     <input type="submit" value="valider">
+
 </form>
 
-<?php
+<?php   
 
-// If 
-    if(!empty($_GET['firstName']) && !empty($_GET['lastName']) && !empty($_GET['gender'])) {
-        $gender = $_GET['gender'];
-        $firstName = $_GET['firstName'];
-        $lastName = $_GET['lastName'];
-        $file = $_GET['file'];
-        $fileType= strtolower(pathinfo($file, PATHINFO_EXTENSION));
+ } else if(!empty($_GET['firstName']) && !empty($_GET['lastName']) && !empty($_GET['gender'])){
 
-    }   else if ($fileType != 'pdf'){
-
-        echo "<br/> Le format doit-être au pdf !<br/>";
-    
-    ?>
-    
-<a href="http://partie7.localhost/Exo8/index.php">Recommencer</a>
-
-<?php
-
-     }  else if($gender == "Homme"){
-         echo "Bonjour ". $firstName ." ". $lastName ." et vous etes un ".$gender ." et votre fichier est  ". $file ;
-     }  else if($gender == "Femme") {
-        echo "Bonjour ". $firstName ." ". $lastName ." et vous etes une ".$gender ." et votre fichier est  ". $file ;
-     }  else if($gender == "autre") {
-        echo "Bonjour ". $firstName ." ". $lastName ." vous etes de type ".$gender ." et votre fichier est  ". $file;
-     }  else {
+     $gender = $_GET['gender'];
+     $firstName = $_GET['firstName'];
+     $lastName = $_GET['lastName'];
+     $file = $_GET['file'];
+     $fileType = strtolower(pathinfo($file,PATHINFO_EXTENSION));
+    $fileNoExt = pathinfo($file , PATHINFO_FILENAME);
+     if($gender == "Homme"){
+         echo "Bonjour ". $firstName ." ". $lastName ." et vous etes un ".$gender;
+     } else if($gender == "Femme") {
+        echo "Bonjour ". $firstName ." ". $lastName ." et vous etes une ".$gender;
+     } else if($gender == "autre") {
+        echo "Bonjour ". $firstName ." ". $lastName ." vous etes de type ".$gender;
+      } else {
             echo "vous n'avez pas mis de sexe ";
         }
-    }
-     
 
-     ?>
+        if($fileType == 'pdf'){
+            echo "<br/>  Votre fichier ".$fileNoExt." est un pdf !";
+        }else{
+            echo "<br/> Veuillez mettre un pdf <br/>";
+    ?>
+<a href="http://partie7.localhost/Exo8/index.php">Revenir au formulaire</a>
+<?php
+        }
+     }
+    
+ ?>
